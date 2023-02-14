0ce38f408db329a5187e766f33c6e9515f9e3922
himadhith <hima130602@gmail.com>
2023-02-14 22:36:26 +0530

    adding full name

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,7 +6,8 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout<<"PES1UG20CS478\n";
-  cout<<"Adding full name: V Himadhith\n";
+  cout<<"Adding full name\n";
+  cout<<"V Himadhith\n";
   cout << "I have successfully built and run\n";
   return 0;
 }
